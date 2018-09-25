f06845542b5423117268e72fd5df1c850222f822
sophianc <sophianygaard@gmail.com>
2018-09-25 10:56:58 +0200

    Forsøg på hjemside

---
+++ hjemside.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head><title>Hjem</title></head>
+<body>
+<h1>MR-Scanning - Region Midt</h1>
+<div class="tabContainer">
+    <div class="buttonContainer">
+        <button onclick="window.location.href='hjemside.php'">Hjem</button>
+        <button onclick="window.location.href='sammenlignscanner.php'">Sammenlign Scannere</button>
+        <button onclick="window.location.href='overblikscannere.php'">Overblik over Scannere</button>
+    </div>
+    <div class="tabPanel"></div>
+    <div class="tabPanel"></div>
+    <div class="tabPanel"></div>
+    </div>
+<?php
+	echo'Hjem';
+?>
+
+
+    <script src="myScripttest.js"></script>
+</body>
+</html>
---
+++ sammenlignscanner.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head><title>Sammenlign scannere</title></head>
+<body>
+    <!doctype html>
+<html>
+<head><title>Hjem</title></head>
+<body>
+<h1>MR-Scanning - Region Midt</h1>
+<div class="tabContainer">
+    <div class="buttonContainer">
+        <button onclick="window.location.href='hjemside.php'">Hjem</button>
+        <button onclick="window.location.href='sammenlignscanner.php'">Sammenlign Scannere</button>
+        <button onclick="window.location.href='overblikscannere.php'">Overblik over Scannere</button>
+    </div>
+
+
+</div>
+    <script src="myScripttest.js"></script>
+
+<?php
+	echo'Sammenlign scannere';
+?>
+</body>
+</html>
