0113977f0bd7e883e5e1be3ac73a0e65a9f19865
Ida Sofie Rasmusen <idasofierasmusen@Idas-MacBook-Pro.local>
2018-09-27 13:10:35 +0200

    Reference er kommet på

--- logout.php
+++ logout.php
@@ -1,4 +1,5 @@
 <?php
+// Koden i denne fil kommer fra hjemmesiden TutorialRepublic hentet den 24.09.2018 via følgende link: https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php.
 
 session_start();
 
