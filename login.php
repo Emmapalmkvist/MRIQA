d117ac0304cec8c01bc5cf9365b31b69832262bf
Emmapalmkvist <palmkvistemma@hotmail.com>
2018-10-03 15:55:56 +0200

    h

--- login.php
+++ login.php
@@ -1,123 +1,41 @@
-<?php
-// Initialize the session
-session_start();
-
-
-// Include config file
-require_once "config.php";
-
-// Define variables and initialize with empty values
-$Brugernavn = $Adgangskode = "";
-$username_err = $password_err = "";
-
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    // Check if username is empty
-    if(empty(trim($_POST["Brugernavn"]))){
-        $username_err = "Indtast venligst brugernavn.";
-    } else{
-        $Brugernavn = trim($_POST["Brugernavn"]);
-    }
-
-    // Check if password is empty
-    if(empty(trim($_POST["Adgangskode"]))){
-        $password_err = "Indtast venligst adgangskode.";
-    } else{
-        $Adgangskode = trim($_POST["Adgangskode"]);
-    }
-
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, Brugernavn, Adgangskode FROM Logind WHERE Brugernavn = ?";
-
-        if($stmt = $mysqli->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bind_param("s", $param_username);
-
-            // Set parameters
-            $param_username = $Brugernavn;
-
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                // Store result
-                $stmt->store_result();
-
-                // Check if username exists, if yes then verify password
-                if($stmt->num_rows == 1){
-                    // Bind result variables
-                    $stmt->bind_result($id, $Brugernavn, $hashed_password);
-                    if($stmt->fetch()){
-
-
-                        // her det går galt
-                        if(password_verify($Adgangskode, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["Brugernavn"] = $Brugernavn;
-
-                            // Redirect user to welcome page
-                            header("location: hjemside.php");
-                            }
-                        else{
-                            // Display an error message if password is not valid
-                            $password_err = "Adgangskoden eller brugernavn er ikke gyldig.";
-                        }
-                    }
-                } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "Adgangskoden eller brugernavn er ikke gyldig.";
-                }
-            } else{
-                echo "Fejl er opstået. Prøv igen.";
-            }
-        }
-
-        // Close statement
-        //$stmt->close();
-    }
-
-    // Close connection
-    $mysqli->close();
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
+<html>
+<head><title>PHP test</title></head>
 <body>
-    <div class="wrapper">
-        <h2>Log ind til MRI-kvalitetssikringssystem</h2>
-        <p>Udfyld venligst dine log ind oplysninger nedenfor.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-                <label>Brugernavn: </label>
-                <input type="text" name="Brugernavn" class="form-control" value="<?php echo $Brugernavn; ?>">
-                <span class="help-block"><?php echo $username_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Adgangskode: </label>
-                <input type="password" name="Adgangskode" class="form-control">
-                <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Log ind">
-            </div>
+<?php
+    echo 'Hej databasen';
+?>
+    </body>
 
-        </form>
-    </div>
-</body>
-</html>
+<?php
+$servername = "5.104.105.222";
+$username = "iha";
+$password = "specialeprojekt";
+$db = "qaspeciale";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db, 8085); // 8085 port
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+$sql = "INSERT INTO qaspeciale.Logind (Brugernavn, Adgangskode)
+VALUES ('sophia', 'kode4567')";
+
+
+$sql = "SELECT Brugernavn, Adgangskode FROM Logind";
+$result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Brugernavn: " . $row["Brugernavn"]. " - Adgangskode: " . $row["Adgangskode"];
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
