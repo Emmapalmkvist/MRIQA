219f9cc43899ed824e723618d820b18ae54384dc
Emmapalmkvist <palmkvistemma@hotmail.com>
2018-10-04 14:03:05 +0200

    billede

--- graftest.php
+++ graftest.php
@@ -176,8 +176,8 @@ var chart6 = new CanvasJS.Chart("chartContainer6", {
 	},
 	data: [{
 		type: "line",
-        toolTipContent:"Dato: {label}<br/> Drift: {y}<br/> Billede: {sti}",
-        name: '<img src= "{sti}" height="120" width="150">',
+        toolTipContent:"Dato: {label}<br/> Drift: {y}<br/> Billede: {name}",
+        name: '<img src= "..{sti}" height="120" width="150">',
 		dataPoints: <?php echo json_encode($drift, JSON_NUMERIC_CHECK); ?>
 	}]
 });
