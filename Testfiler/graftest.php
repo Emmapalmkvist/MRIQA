a4124027072729b08993f48832d262bef6637935
Ida Sofie Rasmusen <idasofierasmusen@Idas-MacBook-Pro.local>
2018-10-09 12:36:23 +0200

    opdeling i mapper

--- Testfiler/graftest.php
+++ Testfiler/graftest.php
@@ -75,26 +75,7 @@ echo '</pre>';
 
 ?>
 
-<?php
 
-
-$dataPoinss = array(
-	array("y" => 25, "label" => "Sunday"),
-	array("y" => 15, "label" => "Monday"),
-	array("y" => 25, "label" => "Tuesday"),
-	array("y" => 5, "label" => "Wednesday"),
-	array("y" => 10, "label" => "Thursday"),
-	array("y" => 0, "label" => "Friday"),
-	array("y" => 20, "label" => "Saturday")
-);
-
-/*
-echo '<pre>';
-print_r($dataPoints);
-echo '</pre>';
-*/
-
-?>
 <!DOCTYPE HTML>
 <html>
 <head>
